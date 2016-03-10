0857d5df51d13286eadbbb9fa968cfc2ff0acb91
Jeff Eng <jeff@jeffeng.me>
2016-03-09 23:53:39 -0800

    modified constructor function properties as well as object properties.

--- scripts/project.js
+++ scripts/project.js
@@ -2,25 +2,23 @@ var articles = [];
 
 function Project (opts) {
   this.projectName = opts.projectName;
-  this.author = opts.author;
+  this.developer = opts.developer;
   this.category = opts.category;
-  // this.authorUrl = opts.authorUrl;
   this.publishedOn = opts.publishedOn;
+  this.githubRepoUrl = opts.githubRepoUrl;
   this.body = opts.body;
 }
 
 Project.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+
   $newArticle.attr('data-category', this.category);
   $newArticle.find('h1').html(this.projectName);
-  $newArticle.find('address').html(this.author);
+  $newArticle.find('address').html(this.developer);
   $newArticle.find('.article-body').html(this.body);
-  // $newArticle.find('address a').attr('href', this.authorUrl);
 
-  // Include the publication date as a 'title' attribute to show on hover:
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
 
-  // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
   $newArticle.append('<hr>');
@@ -35,7 +33,7 @@ rawData.sort(function(a,b) {
 });
 
 rawData.forEach(function(ele) {
-  articles.push(new Article(ele));
+  articles.push(new Project(ele));
 });
 
 articles.forEach(function(a){
