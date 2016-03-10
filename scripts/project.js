b81a18b53539e2d1f0d38bd2f246859c56e0e434
Jeff Eng <jeff@jeffeng.me>
2016-03-09 22:50:16 -0800

    created project constructor function and testing with one project object. able to display on page correctly with a placeholder img

--- scripts/project.js
+++ scripts/project.js
@@ -1,10 +1,43 @@
-var projects = [];
+var articles = [];
 
 function Project (opts) {
-  this.title = opts.title;
-  this.description = opts.description;
+  this.projectName = opts.projectName;
+  this.author = opts.author;
+  this.category = opts.category;
+  // this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Project.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.category);
+  $newArticle.find('h1').html(this.projectName);
+  $newArticle.find('address').html(this.author);
+  $newArticle.find('.article-body').html(this.body);
+  // $newArticle.find('address a').attr('href', this.authorUrl);
 
+  // Include the publication date as a 'title' attribute to show on hover:
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+
+  // Display the date as a relative number of "days ago":
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+
+  $newArticle.append('<hr>');
+
+  $newArticle.removeClass('template');
+
+  return $newArticle;
 };
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml());
+});
