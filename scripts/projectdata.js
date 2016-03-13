2632f1504fc31a2866b6074c57adaf97704dc783
Jeff Eng <jeff@jeffeng.me>
2016-03-12 18:52:43 -0800

    added content to about and contact sections. also added in ipsum text for projects. included icomoon font graphics. added font style lato

--- scripts/projectdata.js
+++ scripts/projectdata.js
@@ -5,7 +5,7 @@ var rawData = [
     developer:     'Ben Garnaat, Zachary Simonson, Sam Gruse, and Jeff Eng',
     githubRepoUrl: 'https://github.com/bgarnaat/starving_artists',
     createdOn:     '2016-01-29',
-    body:          '<p>This is descriptive text about this project and will fill in details at a later time.</p><p><img src="https://placeholdit.imgix.net/~text?txtsize=38&bg=333333&txt=400%C3%97300&w=400&h=300"></p>'
+    body:          '<p>At nolo de me ipso turpis. Ut nullam curae. Scis quid mihi quod infernum sit amet nunc magis animum, nunc eros eget quam cogitatione emptio damnat. Purus? Sic, si fieri justi ... amabo.</p><p><img src="https://placeholdit.imgix.net/~text?txtsize=38&bg=333333&txt=400%C3%97300&w=400&h=300"></p>'
   },
   {
     projectName:   'Campfire Coffee',
@@ -13,7 +13,7 @@ var rawData = [
     developer:     'Jeff Eng',
     githubRepoUrl: 'https://github.com/jeff-eng/campfire-coffee',
     createdOn:     '2016-01-14',
-    body:          '<p>This is descriptive text about this project and will fill in details at a later time.</p><p><img src="https://placeholdit.imgix.net/~text?txtsize=38&bg=333333&txt=400%C3%97300&w=400&h=300"></p>'
+    body:          '<p>At nolo de me ipso turpis. Ut nullam curae. Scis quid mihi quod infernum sit amet nunc magis animum, nunc eros eget quam cogitatione emptio damnat. Purus? Sic, si fieri justi ... amabo.</p><p><img src="https://placeholdit.imgix.net/~text?txtsize=38&bg=333333&txt=400%C3%97300&w=400&h=300"></p>'
   },
   {
     projectName:   'BusMall',
@@ -21,6 +21,6 @@ var rawData = [
     developer:     'Jeff Eng',
     githubRepoUrl: 'https://github.com/jeff-eng/busmall',
     createdOn:     '2016-01-21',
-    body:          '<p>This is descriptive text about this project and will fill in details at a later time.</p><p><img src="https://placeholdit.imgix.net/~text?txtsize=38&bg=333333&txt=400%C3%97300&w=400&h=300"></p>'
+    body:          '<p>At nolo de me ipso turpis. Ut nullam curae. Scis quid mihi quod infernum sit amet nunc magis animum, nunc eros eget quam cogitatione emptio damnat. Purus? Sic, si fieri justi ... amabo.</p><p><img src="https://placeholdit.imgix.net/~text?txtsize=38&bg=333333&txt=400%C3%97300&w=400&h=300"></p>'
   },
 ];
