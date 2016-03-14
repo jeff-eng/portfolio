3b58030e9253c520b19dbafeee713fa5e123ab12
Jeff Eng <jeff@jeffeng.me>
2016-03-14 09:12:55 -0700

    commented out filters and input elements. tabbed sections functional

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,14 +1,40 @@
 var projectView = {};
 
+// Fades in only the tab that was clicked and hides the rest
 projectView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-sections').hide();
-    $('#' + $(this).data('content')).fadeIn();
+    $('#' + $(this).data('navigation')).fadeIn();
   });
 
   $('.main-nav .tab:first').click();
 };
 
+//Populates the Filters
+projectView.populateFilters = function() {
+  $('article').each(function() {
+    if(!$(this).hasClass('template')) {
+      var value = $(this).find('span').text();
+      var optionEl = 'option value="' + value + '">' + value + '</option>';
+      $('#category-filter').append(optionEl);
+    }
+  });
+};
+
+// //Filters the projects by individual or team categories
+// projectView.handleCategoryFilter = function() {
+//   $()
+// };
+//
+// //Filters the projects by project name
+// projectView.handleProjectNameFilter = function() {
+//
+// };
+
+//Calls all the functions once the DOM has loaded
 $(document).ready(function() {
+  projectView.populateFilters();
+  projectView.handleCategoryFilter();
+  projectView.handleProjectNameFilter();
   projectView.handleMainNav();
 });
