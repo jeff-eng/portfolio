bdc5f2e89dc67756261d3c6dc9644cc1d544a31b
Jeff Eng <jeff@jeffeng.me>
2016-03-15 23:23:53 -0700

    remove commented out code from project.js and modules.css files

--- scripts/project.js
+++ scripts/project.js
@@ -10,18 +10,6 @@ Project.prototype.toHtml = function() {
   var template = Handlebars.compile($('#project-template').html())
   this.publishtime = 'about ' + parseInt((new Date() - new Date(this.createdOn))/60/60/24/1000) + ' days ago';
   return template(this);
-  // var $newArticle = $('article.template').clone();
-  // $newArticle.removeClass('template');
-
-  // $newArticle.attr('data-category', this.category);
-  // $newArticle.attr('data-project', this.projectName);
-  // $newArticle.find('h3').html(this.projectName);
-  // $newArticle.find('span').html(this.developer);
-  // $newArticle.find('.article-body').html(this.body);
-  // $newArticle.find('time[pubdate]').attr('title', this.createdOn);
-  // $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.createdOn))/60/60/24/1000) + ' days ago');
-  // $newArticle.append('<hr>');
-  // return $newArticle;
 };
 
 projectData.sort(function(a,b) {
