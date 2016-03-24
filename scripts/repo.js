c9aee885f8fc9a81f6d9c18db43628d09596716a
Jeff Eng <jeff@jeffeng.me>
2016-03-23 17:24:50 -0700

    refactored ajax call using the .get jquery method

--- scripts/repo.js
+++ scripts/repo.js
@@ -4,15 +4,13 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/jeff-eng/repos?per_page=3&sort=updated',
-      type: 'GET',
-      headers: {'Authorization': 'token ' + githubToken},
-      success: function(data, message, xhr) {
-        repos.all = data;
-        console.log(data);
-      }
-    }).done(callback);
+    $.get('github/users/jeff-eng/repos' +
+            '?per_page=50' +
+            '&sort=updated')
+        .done(function(data, message, xhr) {
+          repos.all = data;
+        })
+        .done(callback);
   };
 
   repos.with = function(attr) {
