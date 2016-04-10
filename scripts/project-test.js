4ba57ad090a6937003c42e635a1ea71d1e952b0f
Jeff Eng <jeff@jeffeng.me>
2016-04-09 18:42:00 -0700

    added truncateTable and insertRecord methods to the Project object

---
+++ scripts/project-test.js
@@ -0,0 +1,45 @@
+(function(module) {
+  function Project (opts) {
+    Object.keys(opts).forEach(function(e, index, keys) {
+      this[e] = opts[e];
+    },this);
+  }
+
+  Project.all = [];
+
+  Project.createTable = function(callback) {
+    webDB.execute(
+      'CREATE TABLE IF NOT EXISTS projects (' +
+        'id INTEGER PRIMARY KEY, ' +
+        'projectName VARCHAR(255) NOT NULL, ' +
+        'category VARCHAR(20), ' +
+        'developer VARCHAR(255) NOT NULL' +
+        'githubRepoUrl VARCHAR (255), ' +
+        'createdOn DATETIME, ' +
+        'body TEXT NOT NULL);',
+      callback
+    );
+  };
+
+  Project.truncateTable = function(callback) {
+    webDB.execute(
+      'DELETE FROM projects;',
+      callback
+    );
+  };
+
+  Project.prototype.insertRecord = function(callback) {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO projects (projectName, category, developer, githubRepoUrl, createdOn, body) VALUES (?, ?, ?, ?, ? ,?);',
+          'data': [this.projectName, this.category, this.developer, this.githubRepoUrl, this.createdOn, this.body],
+        }
+      ],
+      callback
+    );
+  };
+
+
+  module.Project = Project;
+})(window);
